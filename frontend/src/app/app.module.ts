f31e3603724759225ed2284c40c11a4aabe8c1c9
NOUMBISSIE <noumbissiej@proton.me>
2025-09-05 08:59:23 +0100

    creation des dossiers, les fichiers et mis à jour le fichier app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PersonalInfoComponent } from './form/personal-info/personal-info.component'; 
+import { OfficialDocumentsComponent } from './form/official-documents/official-documents.component'; 
+import { AcademicInfoComponent } from './form/academic-info/academic-info.component'; 
+import { ContactInfoComponent } from './form/contact-info/contact-info.component'; 
 import { StepperComponent } from './stepper/stepper.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     PersonalInfoComponent,
+    OfficialDocumentsComponent, 
+    AcademicInfoComponent, 
+    ContactInfoComponent, 
     StepperComponent
   ],
   imports: [
